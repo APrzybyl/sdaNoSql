0776543a6eea16f58e53a2ab8e86c1c3aaa16ff8
Arkadiusz Przybyl <arkadiusz.przybyl@gmail.com>
2018-04-22 14:12:32 +0200

    Zadanie

---
+++ nested.js
@@ -0,0 +1,17 @@
+ db = connect("localhost:27017/films")
+ 
+  
+ db.film.insert({"title": "Prometeusz",
+		"year": 2015,
+		"director": {
+			"first_name": "Ridley",
+			"last_name": "Scott",
+			"birthday": "1980-11-23"}});
+ 
+ db.film.find({"director.last_name": "Scott"})
+ 
+ //cursor = db.film.find()
+ 
+ // while (cursor.hasNext()) {
+ // printjson(cursor.next());
+ //}
